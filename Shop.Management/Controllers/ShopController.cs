dffd07175e6a2ae10c1443b8cfe31b421ae1883e
MariusMK1 <marius4556@gmail.com>
2022-05-16 21:29:46 +0300

    Started working on Shop.Management

---
+++ Shop.Management/Controllers/ShopController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Management.Models;
+using Shop.Management.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Management.Controllers
+{
+    public class ShopController : Controller
+    {
+        private ShopService _shopService;
+        public ShopController(ShopService shopService)
+        {
+            _shopService = shopService;
+        }
+        public IActionResult Index()
+        {
+            List<ShopItem> _items = _shopService.GetAll();
+            return View(_items);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ShopItem item = new ShopItem();
+            return View(item);
+        }
+        [HttpPost]
+        public IActionResult Add(ShopItem item)
+        {
+            _shopService.Add(item);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(string name)
+        {
+            _shopService.Delete(name);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Shop.Management/Services/ShopService.cs
@@ -0,0 +1,31 @@
+﻿using Shop.Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Management.Services
+{
+    public class ShopService
+    {
+        private List<ShopItem> _shopItems;
+        public ShopService()
+        {
+            _shopItems = new List<ShopItem>();
+        }
+
+        public List<ShopItem> GetAll()
+        {
+            return _shopItems;
+        }
+        public void Add(ShopItem item)
+        {
+            _shopItems.Add(item);
+        }
+        public void Delete(string name)
+        {
+            _shopItems = _shopItems.Where(x => x.Name != name).ToList();
+        }
+    }
+}
